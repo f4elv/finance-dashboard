19c209339f0e2a8f99d47a6e3990cc2ffa7221c0
Rafael Vasconcelos <efael1986@gmail.com>
2026-01-14 19:11:58 -0400

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import { DashboardProvider } from "@/context/DashboardContext";
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Finance Dashboard",
+  description: "A dashboard for financial data visualization",
+};
 
 export default function RootLayout({
   children,
